3bb3759acd3957aa09da224c3921968605a2ce6a
princeraval147 <princeraval147@gmail.com>
2024-06-06 14:17:11 +0530

    Prince Raval Website

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -30,7 +30,6 @@ const GetStarted = () => {
                 <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                     <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
                         <h1 className="mb-8 text-3xl text-center">Sign up</h1>
-
                         <form method='GET' onSubmit={handlerSubmit} >
                             <input
                                 value={input.Name}
