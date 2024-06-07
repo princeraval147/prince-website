ea4990b1d52b0cf55fd5e7d7f9f72a9d58e86ffc
princeraval147 <princeraval147@gmail.com>
2024-06-07 21:23:10 +0530

    React Website With Tailwind Css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <>
             <header className="shadow sticky z-50 top-0">
@@ -28,14 +31,37 @@ const Header = () => {
                                 Get started
                             </Link>
                         </div>
+
+                        <div className="block lg:hidden">
+                            <button
+                                onClick={() => setIsOpen(!isOpen)}
+                                className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+                            >
+                                <svg
+                                    className={`fill-current h-5 w-5 ${isOpen ? "hidden" : "block"}`}
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                                </svg>
+                                <svg
+                                    className={`fill-current h-5 w-5 ${isOpen ? "block" : "hidden"}`}
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+                                </svg>
+                            </button>
+                        </div>
+
                         <div
-                            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                            id="mobile-menu-2"
+                            className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "block" : "hidden"}`}
                         >
-                            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                                <li>
+                            <ul className="flex justify-center flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 lg:flex-grow">
+                                <li className='block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4'>
                                     <NavLink
                                         to="/"
+                                        onClick={() => setIsOpen(!isOpen)}
                                         className={({ isActive }) =>
                                             `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-grey-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                         }
@@ -43,9 +69,10 @@ const Header = () => {
                                         Home
                                     </NavLink>
                                 </li>
-                                <li>
+                                <li className='block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4'>
                                     <NavLink
                                         to="/about"
+                                        onClick={() => setIsOpen(!isOpen)}
                                         className={({ isActive }) =>
                                             `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-grey-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                         }
@@ -53,9 +80,10 @@ const Header = () => {
                                         About
                                     </NavLink>
                                 </li>
-                                <li>
+                                <li className='block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4'>
                                     <NavLink
                                         to="/contact"
+                                        onClick={() => setIsOpen(!isOpen)}
                                         className={({ isActive }) =>
                                             `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-grey-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                         }
@@ -63,9 +91,10 @@ const Header = () => {
                                         Contact
                                     </NavLink>
                                 </li>
-                                <li>
+                                <li className='block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4'>
                                     <NavLink
                                         to="/github"
+                                        onClick={() => setIsOpen(!isOpen)}
                                         className={({ isActive }) =>
                                             `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-grey-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                         }
@@ -75,9 +104,10 @@ const Header = () => {
                                 </li>
                             </ul>
                         </div>
+
                     </div>
                 </nav>
-            </header>
+            </header >
         </>
     )
 }
