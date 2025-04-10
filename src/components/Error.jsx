18898b610639096f322f044efd910b8517373ceb
princeraval147 <princeraval147@gmail.com>
2025-04-10 13:41:19 +0530

    Error page Handle

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Error = () => {
   return (
     <div>
-      <h1 className='text-center font-black text-5xl my-24'>404 Page Not Found</h1>
+      <h1 className='text-center font-black text-5xl my-24'>
+        404 Page Not Found
+      </h1>
     </div>
   )
 }
